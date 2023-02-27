c84b58a6c534c6a4d4be5cd8d8e0fd818544c139
Udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-02-27 23:07:05 +0100

    created navbar and headers component with  full functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar h-14 bg-gray-600 flex justify-center">
+      <div className="navContainer w-full max-w-6xl text-white flex items-center justify-between">
+        <span className="logo font-bold text-red-100">Book$Reserve</span>
+        <div className="navItems">
+          <button className="navButton mr-5 border-none px-5 py-2 cursor-pointer bg-orange-700 rounded">
+            Register
+          </button>
+          <button className="navButton ml-5 border-none px-5 py-2 cursor-pointer bg-orange-700 rounded">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
